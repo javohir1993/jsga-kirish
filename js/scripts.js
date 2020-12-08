4da4b53d1a3360235adee6bee352e352f887367a
javohir1993 <javohirchegabayev@gmail.com>
2020-12-08 16:55:07 +0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var name = prompt(`Ismingni ayt dedim`);
+var surname = prompt('Endi familiyangni ayt');
+var phone = prompt('Telefoning bormi? Bor bo`lsa aytda');
+var pasport = prompt('Pasportingni raqamini TO`G`RI kirit');
+var dollor = prompt('Kalboshdan NECHTA endi bratish?');
+var som = 10443;
+
+
+console.log(`${name} ${surname}. Telefon: ${phone}. Pasport raqami: ${pasport}. Ayirboshlash uchun ${dollor * 100} berildi. So'mda ${dollor * 100 * som} so'm bo'ladi.`);
